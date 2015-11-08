25196d644982ef1bfc8f8bfccb4a11baefdc8d15
Wang Kun <woshibendan2018@hotmail.com>
2015-11-08 21:45:19 +0800

    add the latest papers on arXiv 11/08/2015 Sun

--- update-arxiv.bat
+++ update-arxiv.bat
@@ -1,4 +1,6 @@
+D:
+cd "D:\workspaces\repositories\kun-wang.github.io"
 set info=add the latest papers on arXiv %date%
 git add -A
-git commit -m " %info% "
+git commit -m "%info%"
 git push -u origin master
